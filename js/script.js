11d409c6aaf01be72a947808512b42272e6c9fda
isma-91 <isma91@hotmail.it>
2022-11-02 15:56:55 +0100

    Add difficult select

--- js/script.js
+++ js/script.js
@@ -1,13 +1,58 @@
 const eleGrid = document.querySelector(".grid");
-let diff = document.querySelector("[name=diff]").value;
+const eleBtnPlay = document.querySelector(".btn-play");
 
-for (let i = 1; i <= 100; i++) {
-  const eleSquare = document.createElement("div");
-  eleSquare.innerHTML += [i];
-  eleSquare.classList.add("square");
-  eleGrid.append(eleSquare);
+eleBtnPlay.addEventListener("click", function () {
+  let diff = document.querySelector("[name=diff]").value;
 
-  eleSquare.addEventListener("click", function () {
-    this.classList.toggle("select");
-  });
-}
+  if (diff === "easy") {
+    for (let i = 1; i <= 100; i++) {
+      const eleSquare = document.createElement("div");
+      eleSquare.innerHTML += [i];
+      eleSquare.classList.add("square");
+      // eleSquare.classList.add("easy");
+      eleSquare.style.width = "calc(100% / 10)";
+      eleGrid.append(eleSquare);
+
+      eleSquare.addEventListener("click", function () {
+        this.classList.toggle("select");
+      });
+    }
+  } else if (diff === "normal") {
+    for (let i = 1; i <= 81; i++) {
+      const eleSquare = document.createElement("div");
+      eleSquare.innerHTML += [i];
+      eleSquare.classList.add("square");
+      // eleSquare.classList.add("normal");
+      eleSquare.style.width = "calc(100% / 9)";
+      eleGrid.append(eleSquare);
+
+      eleSquare.addEventListener("click", function () {
+        this.classList.toggle("select");
+      });
+    }
+  } else if (diff === "hard") {
+    for (let i = 1; i <= 49; i++) {
+      const eleSquare = document.createElement("div");
+      eleSquare.innerHTML += [i];
+      eleSquare.classList.add("square");
+      // eleSquare.classList.add("hard");
+      eleSquare.style.width = "calc(100% / 7)";
+      eleGrid.append(eleSquare);
+
+      eleSquare.addEventListener("click", function () {
+        this.classList.toggle("select");
+      });
+    }
+  }
+});
+
+// for (let i = 1; i <= 100; i++) {
+//   const eleSquare = document.createElement("div");
+//   eleSquare.innerHTML += [i];
+//   eleSquare.classList.add("square");
+//   eleGrid.append(eleSquare);
+
+//   eleSquare.addEventListener("click", function () {
+//     this.classList.toggle("select");
+//   });
+// }
